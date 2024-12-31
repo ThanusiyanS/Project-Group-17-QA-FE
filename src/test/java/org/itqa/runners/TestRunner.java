869855ce91bfa0078b8178c0b3b732857316fac2
Vinuja-L <lavavinuja@gmail.com>
2024-12-31 13:19:38 +0530

    Search functionality UT test case added

--- src/test/java/org/itqa/runners/TestRunner.java
+++ src/test/java/org/itqa/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/checkSearch.feature",
         glue = {"org.itqa.steps"},
         plugin = {"pretty", "html:target/cucumber-reports"},
         monochrome = true
---
+++ src/test/java/org/itqa/utills/Constants.java
@@ -0,0 +1,8 @@
+package org.itqa.utills;
+
+public class Constants {
+
+	public static final long EXPLICIT_TIMEOUT = 5;
+	public static final long WAITING_TIME_OVER_HIGH = 5000;
+
+}
