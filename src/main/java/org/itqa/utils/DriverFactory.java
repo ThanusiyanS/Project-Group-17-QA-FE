810d0505b9a99631e66b892bce0c331290b6908c
ThanusiyanS <thanusiyan295@gmail.com>
2025-01-01 20:22:09 +0530

    External driver dependency added

--- src/main/java/org/itqa/utils/DriverFactory.java
+++ src/main/java/org/itqa/utils/DriverFactory.java
@@ -1,5 +1,6 @@
 package org.itqa.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,33 +11,25 @@ public class DriverFactory {
     private static WebDriver webDriver;
 
     // Singleton Pattern for WebDriver
+
     public static void initializeDriver() {
         if (webDriver == null) {
             ChromeOptions options = new ChromeOptions();
 
-            // Check if running in CI environment
-            if (System.getenv("CI") != null) {
+
                 // CI-specific options
                 options.addArguments("--headless=new");
-                options.addArguments("--no-sandbox");
-                options.addArguments("--disable-dev-shm-usage");
-                options.addArguments("--disable-gpu");
                 options.addArguments("--window-size=1920,1080");
                 options.addArguments("--remote-allow-origins=*");
-            }
 
-            System.setProperty("webdriver.chrome.driver", "/Users/thanusiyans/Documents/University/L4S1/ITQA/chromedriver-mac-arm64/chromedriver");
-            webDriver = new ChromeDriver(options);
+
+            WebDriverManager.chromedriver().setup();
+            WebDriver webDriver = new ChromeDriver(options);
             webDriver.manage().window().maximize();
             webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-
+            setWebDriver(webDriver);
         }
-
-
-
-
-
     }
 
     public static void setWebDriver(WebDriver webDriver) {
